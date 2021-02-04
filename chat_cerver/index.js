e455d1fb89d3193a549cdc10bb1bfd4ccec6b25c
vadim smoliy <vadimsmoliy@gmail.com>
2021-02-04 16:28:00 +0200

    vf2

--- chat_cerver/index.js
+++ chat_cerver/index.js
@@ -2,17 +2,39 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
 
+let users = ['bot']
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected')
+
+    socket.on('initUser', (userName) => {
+        if (socket.username === userName) {
+            return socket.emit('userInitialized', userName)
+        }
+
+        if (users.includes(userName)) {
+            return socket.emit('nameIsBusy')
+        }
+
+        socket.username = userName
+        users.push(userName)
+
+        socket.emit('userInitialized', userName)
+        io.emit('usersChanged', users)
+    })
 
     socket.on('disconnect', () => {
-        console.log('444')
+        console.log('404');
+
+        users.splice(users.indexOf(socket.username), 1);
+
+        io.emit('usersChanged', users)
     })
-});
+
+    socket.on('newMessage', (msg) => {
+        io.emit('newMessage', msg)
+    })
+})
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
--- client/app/components/chatuser.js
+++ client/app/components/chatuser.js
@@ -1,11 +1,81 @@
 import Component from '@glimmer/component';
-import io from 'socket.io-client';///dist/socket.io';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import io from 'socket.io-client';
 
-export default class ChatuserComponent extends Component {
+
+export default class MessagesListComponent extends Component {
+    @tracked messages = [
+        {
+            userName: 'bot',
+            text: 'Hello',
+        },
+    ]
+    @tracked users = [];
+
+    @tracked userName
+    @tracked inputText
+
     constructor(...args) {
-        super(...args);
+        super(...args)
 
-        console.log('1')
+        this.socket = io('http://localhost:3000')
+
+
+        const userName = localStorage.getItem('userName')
+
+        this.socket.on('nameIsBusy', () => {
+            alert('Це імя вже існує. Введіть інше')
+
+            this.initUserName()
+        })
+
+        this.socket.on('userInitialized', (userName) => {
+            this.userName = userName
+
+            localStorage.setItem('userName', userName)
+        })
+
+        this.socket.on('newMessage', this.updateMessages)
+        this.socket.on('usersChanged', this.changeUsers)
+
+        if (!userName) {
+            this.initUserName()
+        } else {
+            this.socket.emit('initUser', userName)
+        }
     }
-    socket = io('http://localhost:3000');
+
+    initUserName() {
+        const userName = prompt('Введіть ваше імя')
+
+        if (!userName) {
+            return this.initUserName()
+        }
+
+        this.socket.emit('initUser', userName)
+    }
+
+
+
+    @action onSendMessage(e) {
+        e.preventDefault();
+        this.inputText = e.target[0].value;
+        this.socket.emit('newMessage', {
+            userName: this.userName,
+            text: this.inputText,
+        })
+        e.target[0].value = '';
+
+    }
+
+
+    @action updateMessages(msg) {
+        this.messages = [...this.messages, msg]
+    }
+
+    @action changeUsers(newUsers) {
+        this.users = newUsers
+    }
 }
+
